510bcbe7fe683673907f30bf06f9c2da67f521db
Axkeya <axkeya@gmail.com>
2025-11-05 16:27:07 +0500

    Ver1.3.7

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ let episodesURL = [
     "d13e1059bc01acca0fcf638e42810b2f", // 8
     "33f03537dd0de5277925d721af6acfe6", // 9
     "9e5df95c252bf0c264690f45446bfacf", // 10
+    "617bdc39e5cd7dd20a8274014a9b966c", // 11
+    "3bbc385f57d2c3d98f19af0a901d2a18", // 12
+    "1a4cb963ff0f4f8472fa6fbc147fa2c3", // 13
+    "2ea8ee1e540cba7026698f0f0dc2107e", // 14
+    "0fbeafc89adc025f959a32c3ee65831e", // 15
+    "f213dc34bba7a33805da2cc8d8fbda55", // 16
+    "9983517318b359aba6bd3d6c90504dbb", // 17
+    "696f6365499942055a875fdd12ea674a", // 18
+    "c7630b0dd2da0131532aa1aab79eafd1", // 19
+    "99fea74349162f93b94a9e26e9d65aa1", // 20
+    "77fe06d551e5c3b14b1ddcf7bccac75e", // 21
+    "a38086b8716a3e9ff6242035ebc82488", // 22
+    "0e261cfc0486d6ce7324918dcb8724b2", // 23
+    "4abf31339951358a72390d46b54e430e", // 24
 ];
 
 // Функция для обновления плеера и подсветки
@@ -40,7 +54,7 @@ function updatePlayer(selection) {
 }
 
 // Привязываем события
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 24; i++) {
     document.getElementById(`item${i}`).addEventListener("click", function() {
         updatePlayer(i - 1);
     });
